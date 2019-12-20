2d493374536bd104ed9765acc70b0248d5284725
Brad Waye <bradwaye@gmail.com>
2019-12-19 23:55:56 -0500

    first commit

---
+++ staging/sites/default/files/php/twig/5df779c6dd11c_quick-side.html.twig_EHE_V1QGNg-PRoGBsxYGEA-uH/03z5j8a2XveZzOplJdkMKftk39JX8CB8fq7irS7PY5o.php
@@ -0,0 +1,99 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/gavias_colin/templates/page/parts/quick-side.html.twig */
+class __TwigTemplate_b278bee013af4f1cc393cddd4caf855cd6103325bbfbbef3899d322b14f0a114 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 1];
+        $filters = ["escape" => 6];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        if ($this->getAttribute(($context["page"] ?? null), "quick_side", [])) {
+            // line 2
+            echo "  <div class=\"gva-quick-side quick-side\">
+   <div class=\"side-content\">
+       <div class=\"content-inner\">
+         <a href=\"#\" class=\"quick-side-close\"><i class=\"fa fa-times\"></i></a>
+         ";
+            // line 6
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "quick_side", [])), "html", null, true);
+            echo "
+       </div>  
+   </div>
+  </div>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/gavias_colin/templates/page/parts/quick-side.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  63 => 6,  57 => 2,  55 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "themes/gavias_colin/templates/page/parts/quick-side.html.twig", "/home/wayedesi/public_html/staging/themes/gavias_colin/templates/page/parts/quick-side.html.twig");
+    }
+}
